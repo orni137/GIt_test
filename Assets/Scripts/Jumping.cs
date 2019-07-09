1e457ab97cd7a7a943eb2de68600d1f64f2629db
d.soldatenkov <d.soldatenkov@revizto.com>
2019-07-09 17:45:40 +0300

    Jumping sphere

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = transform.localPosition;
+        v.y = Mathf.Sin(5.0f * Time.time);
+        transform.localPosition = v;
+    }
+}

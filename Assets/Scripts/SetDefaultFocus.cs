03566d60980c923770e286181e245847ca8065ce
d.soldatenkov <d.soldatenkov@revizto.com>
2019-07-09 17:22:22 +0300

    First commit

---
+++ Assets/Scripts/SetDefaultFocus.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetDefaultFocus : MonoBehaviour
+{
+    public GameObject defaultFocuse;
+
+    void Start() {
+        SetDefualtFocuse();
+    }
+    public void SetDefualtFocuse() {
+        EventSystem.current.SetSelectedGameObject(defaultFocuse);
+    }
+}

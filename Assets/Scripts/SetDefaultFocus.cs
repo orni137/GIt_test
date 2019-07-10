4d23647a4f8b64459ca5999f465850082643a959
d.soldatenkov <d.soldatenkov@revizto.com>
2019-07-10 16:35:02 +0300

    supermerge

--- Assets/Scripts/SetDefaultFocus.cs
+++ Assets/Scripts/SetDefaultFocus.cs
@@ -5,8 +5,6 @@ public class SetDefaultFocus : MonoBehaviour
 {
     public GameObject defaultFocuse;
 
-    int fromTheRoot02 = 7;
-
     void Start() {
         SetDefualtFocuse();
     }
@@ -15,7 +13,6 @@ public class SetDefaultFocus : MonoBehaviour
 
     int fromTheRoot01 = 4;
     int fromTheRoot02 = 4;
-    int fromTheRoot03 = 4;
     int fromTheRoot04 = 4;
 
     public void SetDefualtFocuse() {
